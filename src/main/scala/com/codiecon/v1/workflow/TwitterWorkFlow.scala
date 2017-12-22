19045b7049c0760236c94235e7f4586a7715afb1
Mani Teja <maniteja0428@bitbucket.org>
2017-12-22 10:53:05 +0530

    removed unnecessory file and final changes for presentation

--- src/main/scala/com/codiecon/v1/workflow/TwitterWorkFlow.scala
+++ src/main/scala/com/codiecon/v1/workflow/TwitterWorkFlow.scala
@@ -1,10 +1,17 @@
 package com.codiecon.v1.workflow
 
+import java.io.{BufferedReader, InputStreamReader}
+
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.http.entity.StringEntity
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.impl.client.HttpClientBuilder
 import org.elasticsearch.client.Client
 import org.slf4j.{Logger, LoggerFactory}
+
+import scala.util.parsing.json.JSON
 
 object TwitterWorkFlow {
 
@@ -20,7 +27,7 @@ object TwitterWorkFlow {
   val DOC_TYPE = "tweet"
   val wolverineAppUrl: String = conf.getString("codiecon.wolverine-datasource")
 
-  def processTweets(mapRecord: Map[String, Any], esClient: Client): Unit ={
+  def processTweets(mapRecord: Map[String, Any], esClient: Client): Unit = {
     logger.info(f"processing tweet: $mapRecord")
     var body = mapRecord.map { case (key, value) => key -> value }
     val text: String = mapRecord.get(TEXT) match {
@@ -29,18 +36,44 @@ object TwitterWorkFlow {
     val idStr: String = mapRecord.get(ID_STR) match {
       case Some(value) => value.toString
     }
-    val sentimentScore:Double = getSentiment(text)
+    val sentimentScore: Double = getSentiment(text)
     body = body + (SENTIMENT_SCORE -> sentimentScore)
     indexDocumentInElastic(indexName = INDEX_NAME, docType = DOC_TYPE, idStr, body, esClient)
   }
 
-  def getSentiment(text:String): Double ={
-    logger.info(f"getting sentiment for text: $text")
-    0.0
+  def getSentiment(text: String): Double = {
+    var sentimentScore: Double = 0.0
+    logger.info(f"getting sentiment for text: $text from the url: $wolverineAppUrl")
+    val entity = new StringEntity(text)
+    val client = HttpClientBuilder.create().build();
+    val request = new HttpPost(wolverineAppUrl);
+    request.setEntity(entity)
+    val response = client.execute(request);
+    logger.debug(f"response from the peoplehum app for the url: $wolverineAppUrl " +
+      f"request: $request, response:$response")
+    val reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+    val responseAsAMap = JSON.parseFull(reader.readLine()).get.asInstanceOf[Map[String, Any]]
+    logger.debug(f"response from the peoplehum app  casted into MAP, responseMap: $responseAsAMap")
+    if( responseAsAMap.get("responseObject") != null){
+      val responseObject = responseAsAMap.get("responseObject").get.asInstanceOf[Map[String, Any]]
+      sentimentScore = responseObject.get("score") match {
+        case Some(value) => {
+          logger.debug(f"got ${responseObject.get("score")} in the response object. " +
+            f"setting value as $value")
+          value.asInstanceOf[Double]
+        }
+        case _ => {
+          logger.debug(f"got ${responseObject.get("score")} in the response object. " +
+            f" setting value as 0.0")
+          0.0
+        }
+      }
+    }
+    sentimentScore
   }
 
-  def indexDocumentInElastic(indexName:String, docType:String, id:String, body: Map[String, Any]
-                             , esClient:Client): Unit ={
+  def indexDocumentInElastic(indexName: String, docType: String, id: String, body: Map[String, Any]
+                             , esClient: Client): Unit = {
     logger.info(f"inserting document in elastic, index: $indexName, docType: $docType, body: $body")
     val jsonBody = objMapper.writeValueAsString(body)
     try {
@@ -48,10 +81,10 @@ object TwitterWorkFlow {
       logger.debug(f"inserted a doc index: $indexName, docType: $docType, body(JSON): $jsonBody" +
         f", response: $response")
       esClient.admin().indices().prepareRefresh(indexName).get()
-    }catch {
+    } catch {
       case ex: Exception => {
         logger.error(f"error inserting doc in elastic, $indexName, docType: $docType, body(JSON): $jsonBody" +
-          f", ex:",  ex)
+          f", ex:", ex)
       }
     }
   }
